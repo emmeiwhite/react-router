d51e0f619274936a3f456745aeb4ea4cf8730f49
emmeiwhite <emmeiwhite@gmail.com>
2018-11-14 19:06:51 +0530

    componentDidCatch life cycle hook, to look for erros

--- src/components/services.js
+++ src/components/services.js
@@ -1,7 +1,40 @@
 import React,{Component} from 'react';
 
+// To throw an error I will create one Test Component and throw error externally
+
+class Test extends Component{
+    constructor(props){
+        super(props);
+
+        throw new Error();
+    }
+}
+
 export default class Blog extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isError:false
+        }
+    }
+    
+    componentDidCatch(){
+        // Whenever an error will occur within Services component or within the children of services, an error will be thrown
+
+        this.setState({
+            isError:true
+        })
+    }
+
     render(){
+
+        if(this.state.isError){
+            return (
+                <h1>An Error occurred which stopped the rendering of services component</h1>
+            )
+        }
+
         return(
             <div>
                 <h1>Our Services</h1>
@@ -16,6 +49,8 @@ export default class Blog extends Component{
                     <li>Sass</li>
                     <li>ES6</li>
                 </ul>
+
+                <Test />
             </div>
         )
     }
